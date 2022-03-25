12e34c08c969a87e98c14206a350232aa502c694
Haku16 <102378337+Haku16@users.noreply.github.com>
2022-03-25 12:38:47 -0400

    Add files via upload

---
+++ ttt.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+int main(){
+    std::cout << "Lets play Tic-Tac-Toe! Enter the grid number to place an X or O! For example, A1.";
+    std::cout << "  A  B  C\n";
+    std::cout << "1\n" << "2\n" << "3";
+}
