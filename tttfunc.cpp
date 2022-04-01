1c6d6c8da355f28c41a6701f1f0dcedac6f16f15
Haku16 <102378337+Haku16@users.noreply.github.com>
2022-04-01 14:23:17 -0400

    Update tttfunc.cpp

--- tttfunc.cpp
+++ tttfunc.cpp
@@ -19,7 +19,31 @@ void ask_o(){
     space[replace] = "O";
     makeboard();
 }
-bool is_filled = false;
-void check_filled(){
-    space[0]!="1" && space[1]!="2" && space[2]!="3" && space[3]!="4" && space[4]!="5" && space[5]!="6" && space[6]!="7" && space[7]!="8" && space[8]!="9" ? is_filled=true : is_filled=false;
+bool has_winner = false;
+void check_winner(){
+    if (space[0]==space[3] && space[3]==space[6] && space[0] != ' '){
+        space[0]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    } else if (space[1]==space[4] && space[4]==space[7] && space[1] != ' '){
+        space[1]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    } else if (space[2]==space[5] && space[5]==space[8] && space[2] != ' '){
+        space[2]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    } else if (space[0]==space[1] && space[1]==space[2] && space[0] != ' '){
+        space[0]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    } else if (space[3]==space[4] && space[4]==space[5] && space[3] != ' '){
+        space[2]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    } else if (space[6]==space[7] && space[7]==space[8] && space[6] != ' '){
+        space[6]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    } else if (space[0]==space[4] && space[4]==space[8] && space[0] != ' '){
+        space[0]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    } else if (space[2]==space[4] && space[4]==space[6] && space[2] != ' '){
+        space[2]=='X' ? winner='X' : winner = 'O';
+        has_winner = true;
+    }
 }
